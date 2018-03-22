981e6ade8be6bcaee65fca3d3dc66e4b98c25de7
Penut Chen <penut85420@gmail.com>
2018-03-23 05:08:04 +0800

    ImgList

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 VERIFY_TOKEN = os.environ['VERIFY_TOKEN']
 bot = Bot(ACCESS_TOKEN)
 
+with open("imglist.txt", "r") as fin:
+	URL = fin.readlines()
+
 @app.route('/', methods=['GET', 'POST'])
 def receive_message():
 	if request.method == 'GET':
@@ -41,13 +44,7 @@ def send_message(recipient_id, response):
 	return "Success"
 
 def get_message():
-	url = ["https://imgur.com/MT2VzHJ", "https://imgur.com/zzbgTMn", "https://imgur.com/uW9SJ3v",
-		"https://imgur.com/96FZqHu", "https://imgur.com/fhepbnD", "https://imgur.com/P9LH7H7",
-		"https://imgur.com/zDd7Gqf", "https://imgur.com/GsH3IdP", "https://imgur.com/LwPIQTY",
-		"https://imgur.com/RdmMoNP", "https://imgur.com/CLwCYMz", "https://imgur.com/pdkSSD8",
-		"https://imgur.com/k5wryWs", "https://imgur.com/Hdn6uQJ", "https://imgur.com/F3gyz9W",
-		"https://imgur.com/QGtisdN"]
-	return url[int(random.random() * len(url))] + ".jpg"
+	return URL[int(random.random() * len(URL))][:-1] + ".jpg"
 
 if __name__ == '__main__':
 	app.run()
